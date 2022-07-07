f2098d496b907399120e9c961d019a1743953731
juliaebert2903 <47864810+juliaebert2903@users.noreply.github.com>
2022-07-07 17:26:20 -0300

    Add files via upload

--- Semana16/src/index.js
+++ Semana16/src/index.js
@@ -13,24 +13,30 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/meu.html');
 });
 
+let pessoas = [];
+
 io.on('connection', (socket) => {
     socket.client.nick = socket.client.id;
+    pessoas.push({
+        id: socket.client.id,
+        nick: socket.client.nick
+    });
     io.emit('chat message', `${socket.client.nick} se conectou`);
-    io.emit('chat message', `${socket.client.nick} estão online! `);
+    
+    let quantidade = pessoas.length;
+    let nicks = "";
+
+    for (i = 0; i < quantidade ; i++){
+         nicks = nicks + " - " + pessoas[i].nick    
+    }
+
+    //alterar o nick quando o user connectar
+
+    io.emit('chat message', `${quantidade} estão online! Nicks: ${nicks}`);
     console.log('a user connected');
+    console.log(pessoas);
     // console.log({ socket });
 
-
-    //criar um vetor de pessoas e compartilhar 
-    
-
-
-
-
-
-
-
-    
 
 
     socket.on('chat message', (msg) => {
@@ -39,13 +45,32 @@ io.on('connection', (socket) => {
     });
 
     socket.on('set nick', (msg) => {
+        // DENTRO DO VETOR DE PESSOAS, UM OBJETO QUE O ID = socket.client.id
         const oldNick = socket.client.nick
         io.emit('chat message', `${oldNick} trocou seu nome para ${msg}`);
+        pessoas = pessoas.map(pessoa => {
+            if (pessoa.id == socket.client.id) {
+                pessoa.nick = msg
+            }
+            return pessoa
+        })
+
         socket.client.nick = msg;
     });
+    
     socket.on('disconnect', (msg) => {
         io.emit('chat message', `${socket.client.nick} se desconectou`);
         console.log('user disconnect');
+        pessoas = pessoas.filter(pessoa => pessoa.id != socket.client.id) 
+
+        let quantidade = pessoas.length;
+        let nicks = "";
+
+        for (i = 0; i < quantidade; i++) {
+            nicks = nicks + " - " + pessoas[i].nick
+        }
+
+        io.emit('chat message', `${quantidade} estão online! Nicks: ${nicks}`);       
     });
 
     
